5a38684153e3efb3b335f239f3343d058da92950
XBigTK13X <xbigtk13x@gmail.com>
2025-09-24 12:28:43 -0500

    Black outlines for accents

--- generate.py
+++ generate.py
@@ -2,6 +2,104 @@ import os
 from typing import Tuple
 from PIL import Image
 
+character_image = './v2/character/snowflake.png'
+
+text_images = []
+for root,dirs,files in os.walk('./v2/text'):
+    for ff in files:
+        file_path = os.path.join(root,ff)
+        text_images.append(file_path)
+
+inputs = [
+    {
+        'name':'snowstream',
+        'background': (253, 111, 1),
+        'accent': {
+            'position': (0,.1)
+        },
+        'splash': {
+            'text':{
+                'position': (-0.025,0)
+            }
+        }
+    },
+    {
+        'name':'snowgroove',
+        'background': (172,3,244)
+    },
+    {
+        'name':'snowpage',
+        'background': (105, 127, 255),
+        'accent': {
+            'position': (0,.05)
+        }
+    },
+    {
+        'name':'snowtome',
+        'background': (255, 255, 255)
+    },
+    {
+        'name':'snowjam',
+        'background': (255, 255, 255)
+    },
+    {
+        'name':'snowcloud',
+        'background': (255, 255, 255)
+    },
+    {
+        'name':'snowblue',
+        'background': (255, 255, 255)
+    }
+]
+
+exports = [
+    {
+        'name': 'appicon',
+        'width': 1000,
+        'height': 1000,
+        'character': {
+            'position': (.25,.25)
+        },
+        'accent': {
+            'position': (.45,.45)
+        },
+        'text': {
+            'position': (.25,.45)
+        },
+        'scale': 1
+    },
+    {
+        'name': 'splash',
+        'width': 1000,
+        'height': 1000,
+        'character': {
+            'position': (.15,.25)
+        },
+        'accent': {
+            'position': (.5,.25)
+        },
+        'text': {
+            'position': (.2,.55)
+        },
+        'scale': 1
+    },
+    {
+        'name': 'tvbanner',
+        'width': 1200,
+        'height': 600,
+                'character': {
+            'position': (.275,.15)
+        },
+        'accent': {
+            'position': (.525,.15)
+        },
+        'text': {
+            'position': (.175,.25)
+        },
+        'scale': .5
+    },
+]
+
 def resize_keep_aspect(img, target_size):
     orig_w, orig_h = img.size
     target_w, target_h = target_size
@@ -24,83 +122,30 @@ def resize_keep_aspect(img, target_size):
 def create_composite(
     background_size:Tuple[int,int],
     background_color:Tuple[int,int,int],
-    image_a_path:str,
-    image_b_path:str,
-    image_c_path:str,
-    size_a:Tuple[int,int],
-    size_b:Tuple[int,int],
-    size_c:Tuple[int,int],
-    pos_a:Tuple[int,int],
-    pos_b:Tuple[int,int],
-    pos_c:Tuple[int,int],
+    character:dict,
+    accent:dict,
+    text:dict,
     output_path:str
 ):
     background = Image.new("RGB", background_size, background_color)
 
-    img_a = Image.open(image_a_path).convert("RGBA")
-    img_b = Image.open(image_b_path).convert("RGBA")
+    character_image = Image.open(character['image_path']).convert("RGBA")
+    account_image = Image.open(accent['image_path']).convert("RGBA")
+    text_image = None
     if not 'appicon' in output_path:
-        img_c = Image.open(image_c_path).convert("RGBA")
+        text_image = Image.open(text['image_path']).convert("RGBA")
 
-    img_a = resize_keep_aspect(img_a, size_a)
-    img_b = resize_keep_aspect(img_b, size_b)
+    character_image = resize_keep_aspect(character_image, character['size'])
+    account_image = resize_keep_aspect(account_image, accent['size'])
     if not 'appicon' in output_path:
-        img_c = resize_keep_aspect(img_c, size_c)
+        text_image = resize_keep_aspect(text_image, text['size'])
 
-    background.paste(img_a, pos_a, img_a)
-    background.paste(img_b, pos_b, img_b)
+    background.paste(character_image, character['position'], character_image)
+    background.paste(account_image, accent['position'], account_image)
     if not 'appicon' in output_path:
-        background.paste(img_c, pos_c, img_c)
+        background.paste(text_image, text['position'], text_image)
 
     background.save(output_path, "PNG")
-
-character_image = './v2/character/snowflake.png'
-
-text_images = []
-for root,dirs,files in os.walk('./v2/text'):
-    for ff in files:
-        file_path = os.path.join(root,ff)
-        text_images.append(file_path)
-
-inputs = [
-    ['snowstream',(219, 158, 44)],
-    ['snowgroove',(172,3,244)],
-    ['snowpage',(105, 127, 255)],
-    ['snowtome',(255, 255, 255)],
-    ['snowjam',(255, 255, 255)],
-    ['snowcloud',(255, 255, 255)],
-    ['snowblue',(255, 255, 255)]
-]
-
-exports = [
-    {
-        'name': 'appicon',
-        'width': 1000,
-        'height': 1000,
-        'pos_a': (.20,.20),
-        'pos_b': (.40,.40),
-        'pos_c': (.25,.45),
-        'scale': 1
-    },
-    {
-        'name': 'splash',
-        'width': 1000,
-        'height': 1000,
-        'pos_a': (.15,.25),
-        'pos_b': (.5,.25),
-        'pos_c': (.2,.55),
-        'scale': 1
-    },
-    {
-        'name': 'tvbanner',
-        'width': 1200,
-        'height': 600,
-        'pos_a': (.33,.15),
-        'pos_b': (.5,.15),
-        'pos_c': (.15,.25),
-        'scale': .5
-    },
-]
 
 export_count = 1
 export_total = len(exports) * len(inputs)
@@ -109,27 +154,48 @@ for export in exports:
     height = export['height']
     scale = export['scale']
     print(f"\nExporting {export['name']}")
-    for input in inputs:        
-        export_dir = os.path.join('./generated',input[0])
+    for input in inputs:
+        export_dir = os.path.join('./generated',input['name'])
         os.makedirs(export_dir,exist_ok=True)
         export_path = os.path.join(export_dir,f"{export['name']}.png")
         print(f"\t({export_count}/{export_total}) {export_path}")
         export_count += 1
+
+        character_position = export['character']['position']
+        if 'character' in input and 'position' in input['character']:
+            character_position = (character_position[0]+input['character']['position'][0],character_position[1]+input['character']['position'][1])
+        character = {
+            'image_path': './v2/character/snowflake.png',
+            'size': (int(width*scale*.33), None),
+            'position': (int(width*character_position[0]), int(height*character_position[1])),
+        }
+
+        accent_position = export['accent']['position']
+        if 'accent' in input and 'position' in input['accent']:
+            accent_position = (accent_position[0]+input['accent']['position'][0],accent_position[1]+input['accent']['position'][1])
+        accent = {
+            'image_path':f"./v2/accent/{input['name']}.png",
+            'size':(int(width*scale*.33), None),
+            'position': (int(width*accent_position[0]), int(height*accent_position[1])),
+        }
+
+        text_position = export['text']['position']
+        if 'text' in input and 'position' in input['text']:
+            text_position = (text_position[0]+input['text']['position'][0],text_position[1]+input['text']['position'][1])
+        if export['name'] in input and 'text' in input[export['name']] and 'position' in input[export['name']]['text']:
+            text_position = (text_position[0]+input[export['name']]['text']['position'][0],text_position[1]+input[export['name']]['text']['position'][1])
+        text = {
+            'image_path':f"./v2/text/{input['name']}.png",
+            'size':(int(width*.65), None),
+            'position': (int(width*text_position[0]), int(height*text_position[1])),
+        }
+
         create_composite(
             background_size=(width, height),
-            background_color=input[1],
-
-            image_a_path='./v2/character/snowflake.png',
-            size_a=(int(width*scale*.33), None),
-            pos_a=(int(width*export['pos_a'][0]), int(height*export['pos_a'][1])),
-
-            image_b_path=f"./v2/accent/{input[0]}.png",
-            size_b=(int(width*scale*.33), None),
-            pos_b=(int(width*export['pos_b'][0]), int(height*export['pos_b'][1])),
-
-            image_c_path=f"./v2/text/{input[0]}.png",
-            size_c=(int(width*.65), None),
-            pos_c=(int(width*export['pos_c'][0]), int(height*export['pos_c'][1])),
-
+            background_color=input['background'],
+            character=character,
+            accent=accent,
+            text=text,
             output_path=export_path
         )
+
